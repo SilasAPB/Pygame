9762514e909013d10a3e8488f0ab5b5d33375366
Silas A. Pereira <silasapb@al.insper.edu.br>
2023-11-01 19:31:25 -0300

    exercicio1

---
+++ jogo/jogo.py
@@ -0,0 +1,31 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((600, 300))
+pygame.display.set_caption('SandmannVille: Terra de Faroeste')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        if event.type== pygame.KEYUP:
+            game=False
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((0, 0, 255))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
