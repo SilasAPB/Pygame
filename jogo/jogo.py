16d8355a9ee183cbd684fa777c0956b8f40124ac
Silas A. Pereira <silasapb@al.insper.edu.br>
2023-11-05 22:09:03 -0300

    fundo

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -5,8 +5,11 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-window = pygame.display.set_mode((600, 300))
+window = pygame.display.set_mode((600, 420))
 pygame.display.set_caption('SandmannVille: Terra de Faroeste')
+
+# ----- Inicia assets
+backgroud=pygame.image.load('jogo/assets/img/fundo1.png').convert()
 
 # ----- Inicia estruturas de dados
 game = True
@@ -23,6 +26,7 @@ while game:
 
     # ----- Gera saídas
     window.fill((0, 0, 255))  # Preenche com a cor branca
+    window.blit(backgroud, (0,0)) #Nosso Fundo
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
