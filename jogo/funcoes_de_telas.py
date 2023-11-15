4e4ac3fcb1ecf082579671cee4023fb145d6312a
Silas A. Pereira <silasapb@al.insper.edu.br>
2023-11-15 16:39:52 -0300

    config

--- jogo/funcoes_de_telas.py
+++ jogo/funcoes_de_telas.py
@@ -1,6 +1,11 @@
 import pygame
-from jogo import *
+from os import path
+
+from jogo1 import *
 from config import *
+
+
+
 pygame.init()
 def init_screen(window):
     clock = pygame.time.Clock()
@@ -24,8 +29,40 @@ def init_screen(window):
 
 
 
-def final_screen(window):
-    pass
+def end_screen(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+
+    # Carrega o fundo da tela inicial
+    background = pygame.image.load(path.join(IMG_DIR, 'fim.png')).convert()
+    background_rect = background.get_rect()
+
+    running = True
+    while running:
+
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False
+
+            if event.type == pygame.KEYUP:
+                state = INIT
+                running = False
+
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+    return state
+
 
 
 
