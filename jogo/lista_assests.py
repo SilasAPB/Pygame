cb3ff36e3cc0b1b96b2d67f11adc81cc8e54998d
enzochristo <enzosc1@al.insper.edu.br>
2023-11-22 17:10:30 -0300

    adição de sprites

--- jogo/lista_assests.py
+++ jogo/lista_assests.py
@@ -3,11 +3,16 @@ import os
 from jogo1 import*
 from config import *
 
-BACKGROUD = 'backgroud'
+BACKGROUND = 'backgroud'
+BACKGROUND2 = 'background2'
+BACKGROUND3 = 'background3'
+BACKGROUND4 = 'background4'
+
 PLAYER1_IMG = 'player_1_img'
 PLAYER2_IMG = 'player_2_img'
 BLOCK1_IMG = 'block_1_img'
 BLOCK2_IMG = 'block_2_img'
+BLOCK3_IMG = 'bloco_3_img '
 BULLET_IMG= 'img_bala'
 BARRA_IMG= 'img_bar'
 MINIMAP1_IMG= 'minimap_1_bar'
@@ -19,8 +24,11 @@ MINIMAP4_IMG= 'minimap_4_bar'
 
 def load_assets():
     assets ={}
-    assets[BACKGROUD]=pygame.image.load(os.path.join(IMG_DIR, 'background.gif')).convert()
-    assets[BACKGROUD]=pygame.transform.scale(assets[BACKGROUD], (WIDTH, HEIGHT))
+    assets[BACKGROUND]=pygame.image.load(os.path.join(IMG_DIR, 'background.gif')).convert()
+    assets[BACKGROUND]=pygame.transform.scale(assets[BACKGROUND], (WIDTH, HEIGHT))
+
+    assets[BACKGROUND3]=pygame.image.load(os.path.join(IMG_DIR, 'background.gif')).convert()
+    assets[BACKGROUND3]=pygame.transform.scale(assets[BACKGROUND], (WIDTH, HEIGHT))
 
     assets[PLAYER1_IMG]=pygame.image.load(os.path.join(IMG_DIR,'player1.png'))
     assets[PLAYER1_IMG]=pygame.transform.scale(assets[PLAYER1_IMG], (PLAYERS_WIDTH, PLAYERS_HEIGHT))
@@ -30,6 +38,12 @@ def load_assets():
 
     assets[BLOCK1_IMG]=pygame.image.load(os.path.join(IMG_DIR,'block.png'))
     assets[BLOCK1_IMG]=pygame.transform.scale(assets[BLOCK1_IMG], (BLOCK_WIDTH, BLOCK_HEIGHT))
+
+    assets[BLOCK3_IMG]=pygame.image.load(os.path.join(IMG_DIR,'Plataforma_mapa1.png'))
+    assets[BLOCK3_IMG]=pygame.transform.scale(assets[BLOCK1_IMG], (BLOCK_WIDTH, BLOCK_HEIGHT))
+    
+
+    
 
     assets[BLOCK2_IMG]=pygame.image.load(os.path.join(IMG_DIR,'block2.png'))
     assets[BLOCK2_IMG]=pygame.transform.scale(assets[BLOCK2_IMG], (BLOCK_WIDTH, BLOCK_HEIGHT))
