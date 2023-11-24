be30e9a5235957c87c2c9c37879d4e60744556dd
enzochristo <enzosc1@al.insper.edu.br>
2023-11-24 19:32:05 -0300

    Letras embaixo das imagems na seleção de player

--- jogo/person_screen.py
+++ jogo/person_screen.py
@@ -14,7 +14,20 @@ def person_screen(window):
     texto_p1 = smallfont.render('Player 1: escolha seu personagem com as teclas W,A,S,D' , True , RED)
     texto_p2 = smallfont.render('Player 2: escolha seu personagem com as setas no canto inferior do teclado.', True , BLUE)
 
+    textop1_a = smallfont.render('P1: A', True , RED)
+    textop2_left = smallfont.render('P2:  <-', True , BLUE)
 
+    textop1_w = smallfont.render('P1: W', True , RED)
+    arrow_up = smallfont.render('^', True , BLUE)
+    textop2_up = smallfont.render('P2: |', True , BLUE)
+
+    textop1_d = smallfont.render('P1: D', True , RED)
+    textop2_right = smallfont.render('P2: ->', True , BLUE)
+
+    textop1_s = smallfont.render('P1: S', True , RED)
+    arrow_down = smallfont.render('v', True , BLUE)
+    textop2_down = smallfont.render('P2: |', True , BLUE)
+    
 
 
     background = pygame.image.load(path.join(IMG_DIR, 'escolha_mapa.png')).convert()
@@ -75,10 +88,25 @@ def person_screen(window):
         window.blit(background, background_rect)
 
 
-        window.blit(assets[PLAYER1_IMG], (WIDTH/8,HEIGHT/8))
-        window.blit(assets[PLAYER3_IMG], (5*WIDTH/8,HEIGHT/8))
-        window.blit(assets[PLAYER4_IMG], (5*WIDTH/8,5*HEIGHT/8))
-        window.blit(assets[PLAYER5_IMG], (WIDTH/8,5*HEIGHT/8))
+        window.blit(assets[PLAYER1_IMG], (WIDTH/8,(HEIGHT/8)+30))
+        window.blit(textop1_a,((WIDTH/8)- 50,(HEIGHT/8)-60))
+        window.blit(textop2_left,((WIDTH/8)-50,(HEIGHT/8)-20))
+        
+        window.blit(assets[PLAYER3_IMG], (5*WIDTH/8,(HEIGHT/8)+30))
+        window.blit(textop1_w,(((5*WIDTH/8)-50,(HEIGHT/8)-60)))
+        window.blit(arrow_up,((5*WIDTH/8)-4,(HEIGHT/8)-20))
+        window.blit(textop2_up,((5*WIDTH/8)-50,(HEIGHT/8)-20))
+
+        window.blit(assets[PLAYER4_IMG], (5*WIDTH/8,(5*HEIGHT/8)+10))
+        window.blit(textop1_d,((5*WIDTH/8)-50,(5*HEIGHT/8)-70))
+        window.blit(textop2_right,(((5*WIDTH/8)-50,(5*HEIGHT/8)-40)))
+
+        window.blit(assets[PLAYER5_IMG], (WIDTH/8,(5*HEIGHT/8)+10))
+        window.blit(textop1_s,((WIDTH/8)-51,(5*HEIGHT/8)-70))
+        window.blit(arrow_down,((WIDTH/8)-3,(5*HEIGHT/8)-19))
+        window.blit(textop2_down,((WIDTH/8)-48.5,(5*HEIGHT/8)-35))
+
+
         window.blit(texto_p1,(5,HEIGHT-30))
         window.blit(texto_p2,(5,HEIGHT-70))
     
