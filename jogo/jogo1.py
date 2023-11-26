b9e540daf6dd4de2f03fc6f240383e5dbf6e4cb6
enzochristo <enzosc1@al.insper.edu.br>
2023-11-26 17:20:06 -0300

    Comentarios feitos no jogo

--- jogo/jogo1.py
+++ jogo/jogo1.py
@@ -20,8 +20,8 @@ def jogo_principal(window,tela,choose1,choose2):
     game = True
 
     playerControls={
-    'p1' : [pygame.K_a,pygame.K_d,pygame.K_w,pygame.K_s,pygame.K_r,pygame.K_q], #humberto
-    'p2' : [pygame.K_LEFT,pygame.K_RIGHT,pygame.K_UP,pygame.K_DOWN,pygame.K_RSHIFT,pygame.K_SLASH]#dani
+    'p1' : [pygame.K_a,pygame.K_d,pygame.K_w,pygame.K_s,pygame.K_r,pygame.K_q], 
+    'p2' : [pygame.K_LEFT,pygame.K_RIGHT,pygame.K_UP,pygame.K_DOWN,pygame.K_RSHIFT,pygame.K_SLASH]
     
     }
 # ----- Criando um grupo de sprites(que vai agir/atualizar conforme o tempo)
