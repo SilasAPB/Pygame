8a81b17dda3f1468cc90ef12698a870ff7d2e3f2
enzochristo <enzosc1@al.insper.edu.br>
2023-11-27 19:15:27 -0300

    Arrumando os parametros das armas

--- jogo/guns.py
+++ jogo/guns.py
@@ -3,13 +3,13 @@ ITEMS = {
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "OBLIQUE",  # Tipo do projétil
         "velocity" : 25,  # Velocidade do projétil
-        "spray" : .2,  # % da variação de ângulo de tiro
+        "spray" : .7,  # % da variação de ângulo de tiro
         "size" : 8,  # Quantidade de projéteis antes de cooldown
-        "cadence" : 5,  # Quantidade de Frames entre os usos do item
+        "cadence" : 12,  # Quantidade de Frames entre os usos do item
         "recoil" : 4,  # Velocidade do recuo da arma
         'reload': 3,  # Cooldown entre a velocidade de recarga da arma
         "soundEffect" : "Som2.wav",
-        "damage": 5,
+        "damage": 2,
         #"useParticle" : "" 
         #"hitParticle" : ""
     },
@@ -17,13 +17,13 @@ ITEMS = {
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "STRAIGHT",  # Tipo do projétil
         "velocity" : 30,  # Velocidade do projétil
-        "spray" : .1,  # % da variação de ângulo de tiro
+        "spray" : .3,  # % da variação de ângulo de tiro
         "size" : 8,  # Quantidade de projéteis antes de cooldown
         "cadence" : 10,  # Quantidade de Frames entre os usos do item
-        'reload': 30,  # Cooldown entre a velocidade de recarga da arma
+        'reload': 20,  # Cooldown entre a velocidade de recarga da arma
         "recoil" : 4,  # Velocidade do recuo da arma
         "soundEffect" : "Som1.wav",
-        "damage": 5,
+        "damage": 2,
         "useParticle" : "",
         "hitParticle" : ""
     },
@@ -31,55 +31,55 @@ ITEMS = {
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "BOUNCE",  # Tipo do projétil
         "velocity" : 15,  # Velocidade do projétil
-        "spray" : .3,  # % da variação de ângulo de tiro
+        "spray" : .1,  # % da variação de ângulo de tiro
         "size" : 8,  # Quantidade de projéteis antes de cooldown
         "cadence" : 15,  # Quantidade de Frames entre os usos do item
         'reload': 30,  # Cooldown entre a velocidade de recarga da arma
-        "recoil" : 4,  # Velocidade do recuo da arma
+        "recoil" : 4.5,  # Velocidade do recuo da arma
         "soundEffect" : "Som1.wav",
-        "damage": 9,
+        "damage": 2,
         "useParticle" : "",
         "hitParticle" : ""
     },
      "Arma 4":{
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "STRAIGHT",  # Tipo do projétil
-        "velocity" : 20,  # Velocidade do projétil
-        "spray" : .9,  # % da variação de ângulo de tiro
+        "velocity" : 15,  # Velocidade do projétil
+        "spray" : .8,  # % da variação de ângulo de tiro
         "size" : 20,  # Quantidade de projéteis antes de cooldown
         "cadence" : 18,  # Quantidade de Frames entre os usos do item
         'reload': 30,  # Cooldown entre a velocidade de recarga da arma
         "recoil" : 8.5,  # Velocidade do recuo da arma
         "soundEffect" : "Som1.wav",
-        "damage": 10,
+        "damage": 2,
         "useParticle" : "",
         "hitParticle" : ""
     }, 
     "Arma 5":{
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "OBLIQUE",  # Tipo do projétil
-        "velocity" : 10,  # Velocidade do projétil
+        "velocity" : 15,  # Velocidade do projétil
         "spray" : .9,  # % da variação de ângulo de tiro
         "size" : 15,  # Quantidade de projéteis antes de cooldown
-        "cadence" : 15,  # Quantidade de Frames entre os usos do item
-        'reload': 30,  # Cooldown entre a velocidade de recarga da arma
-        "recoil" : 10,  # Velocidade do recuo da arma
+        "cadence" : 12,  # Quantidade de Frames entre os usos do item
+        'reload': 25,  # Cooldown entre a velocidade de recarga da arma
+        "recoil" : 8,  # Velocidade do recuo da arma
         "soundEffect" : "Som2.wav",
-        "damage": 9,
+        "damage": 4,
         "useParticle" : "",
         "hitParticle" : ""
     },
      "Arma 6":{
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "BOUNCE",  # Tipo do projétil
-        "velocity" : 35,  # Velocidade do projétil
+        "velocity" : 30,  # Velocidade do projétil
         "spray" : .8,  # % da variação de ângulo de tiro
         "size" : 15,  # Quantidade de projéteis antes de cooldown
         "cadence" : 5,  # Quantidade de Frames entre os usos do item
         'reload': 30,  # Cooldown entre a velocidade de recarga da arma
         "recoil" : 10,  # Velocidade do recuo da arma
         "soundEffect" : "Som3.wav",
-        "damage": 5,
+        "damage": 3.5,
         "useParticle" : "",
         "hitParticle" : ""
     },
@@ -100,28 +100,28 @@ ITEMS = {
      "Arma 8":{
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "BOUNCE",  # Tipo do projétil
-        "velocity" : 40,  # Velocidade do projétil
+        "velocity" : 20,  # Velocidade do projétil
         "spray" : .8,  # % da variação de ângulo de tiro
         "size" : 20,  # Quantidade de projéteis antes de cooldown
         "cadence" : 9,  # Quantidade de Frames entre os usos do item
-        'reload': 30,  # Cooldown entre a velocidade de recarga da arma
+        'reload': 25,  # Cooldown entre a velocidade de recarga da arma
         "recoil" : 2,  # Velocidade do recuo da arma
         "soundEffect" : "Som3.wav",
-        "damage": 7.2,
+        "damage": 4.2,
         "useParticle" : "",
         "hitParticle" : ""
     },
      "Arma 9":{
         "asset" : "../assets/img/pistola.png",  # Imagem do sprite da arma
         "itemType" : "OBLIQUE",  # Tipo do projétil
-        "velocity" : 40,  # Velocidade do projétil
-        "spray" : .0,  # % da variação de ângulo de tiro
+        "velocity" : 30,  # Velocidade do projétil
+        "spray" : .8,  # % da variação de ângulo de tiro
         "size" : 30,  # Quantidade de projéteis antes de cooldown
         "cadence" : 25,  # Quantidade de Frames entre os usos do item
-        'reload': 30,  # Cooldown entre a velocidade de recarga da arma
-        "recoil" : 20,  # Velocidade do recuo da arma
+        'reload': 10,  # Cooldown entre a velocidade de recarga da arma
+        "recoil" : 8,  # Velocidade do recuo da arma
         "soundEffect" : "Som2.wav",
-        "damage": 7.2,
+        "damage": 4.2,
         "useParticle" : "",
         "hitParticle" : ""
     },
