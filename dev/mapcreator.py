fcbc6c0754214a4529d465ffc2f5f2e668400cab
YamanduGermano <yamandu.germano@gmail.com>
2023-11-29 02:18:34 -0300

    Editor de mapas mudando tileset

--- dev/mapcreator.py
+++ dev/mapcreator.py
@@ -12,6 +12,14 @@ print("""[ EDITOR DE MAPAS ]
 
 - Crie um arquivo de mapa novo
 - Especifique o nome de um já existente para modificá-lo\n""")
+
+print("""Background existentes:
+1 - Templo Guangzhou.json
+2 - Jardins Cristalinos
+3 - Canyon dos Esquecidos
+4 - Beco do Coringa.json""")
+
+mapBG = int(input("Defina o background do mapa (1 a 4): "))-1
 mapName = input("Digite o nome do mapa: ")
 
 modo = "NEW"
@@ -61,7 +69,7 @@ def printTable(table):
 
 
 
-cursor = Block(assets[BLOCK1_MAP2],0,0)
+cursor = Block(assets[MAPS[mapBG][0]],0,0)
 all_sprites.add(cursor)
 
 if modo == "NEW":
@@ -84,14 +92,13 @@ gameMap = []
 for ln in gamePlainMap:
     gameMap.append(list(ln))
 
-blockselection = BLOCK1_MAP2
-blockSymbol = 1
+blockSymbol = 0
 
 # Inicializa o mapa
 for ln in range(len(gameMap)):
     for col in range(len(gameMap[ln])):
         if gameMap[ln][col] != 0:
-            gameMap[ln][col] = Block(assets[blockselection],col*TILESIZE,ln*TILESIZE)
+            gameMap[ln][col] = Block(assets[MAPS[mapBG][gameMap[ln][col]-1]],col*TILESIZE,ln*TILESIZE)
             all_sprites.add(gameMap[ln][col])
 
 # printTable(gamePlainMap)
@@ -105,7 +112,7 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
             if gameMap[mouseY][mouseX] == 0 and cursor != 0:
-                gameMap[mouseY][mouseX] = Block(assets[blockselection],mouseX*TILESIZE,mouseY*TILESIZE)
+                gameMap[mouseY][mouseX] = Block(assets[MAPS[mapBG][blockSymbol-1]],mouseX*TILESIZE,mouseY*TILESIZE)
                 all_sprites.add(gameMap[mouseY][mouseX])
                 gamePlainMap[mouseY][mouseX] = blockSymbol
 
@@ -120,27 +127,27 @@ while game:
             if cursor != 0:
                 cursor.kill()
             if event.key == pygame.K_0 and cursor != 0:
-                blockselection = 0
+                # blockselection = 0
                 cursor = 0
                 blockSymbol = 0
                 print(0)
             if event.key == pygame.K_1:
-                blockselection = BLOCK1_MAP2
-                cursor = Block(assets[blockselection],mouseX*TILESIZE,mouseY*TILESIZE)
-                all_sprites.add(cursor)
+                # blockselection = BLOCK1_MAP2
                 blockSymbol = 1
+                cursor = Block(assets[MAPS[mapBG][blockSymbol-1]],mouseX*TILESIZE,mouseY*TILESIZE)
+                all_sprites.add(cursor)
                 print(1)
             if event.key == pygame.K_2:
-                blockselection = BLOCK2_MAP2
-                cursor = Block(assets[blockselection],mouseX*TILESIZE,mouseY*TILESIZE)
-                all_sprites.add(cursor)
+                # blockselection = BLOCK2_MAP2
                 blockSymbol = 2
+                cursor = Block(assets[MAPS[mapBG][blockSymbol-1]],mouseX*TILESIZE,mouseY*TILESIZE)
+                all_sprites.add(cursor)
                 print(2)
             if event.key == pygame.K_3:
-                blockselection = BLOCK3_MAP2
-                cursor = Block(assets[blockselection],mouseX*TILESIZE,mouseY*TILESIZE)
-                all_sprites.add(cursor)
+                # blockselection = BLOCK3_MAP2
                 blockSymbol = 3
+                cursor = Block(assets[MAPS[mapBG][blockSymbol-1]],mouseX*TILESIZE,mouseY*TILESIZE)
+                all_sprites.add(cursor)
                 print(3)
         if event.type == pygame.QUIT:
             game = False
